f9342d9ef9bd60babdc022ad1890381f9278ddcc
Tejaswi Yerukalapudi <tejaswi.yvs@gmail.com>
2015-08-12 11:15:46 -0400

    foo

--- app/src/main/java/tejaswi_yerukalapudi/com/pinnabletableview/MainActivity.java
+++ app/src/main/java/tejaswi_yerukalapudi/com/pinnabletableview/MainActivity.java
@@ -1,10 +1,13 @@
 package tejaswi_yerukalapudi.com.pinnabletableview;
 
 import android.app.Activity;
+import android.content.Context;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.TypedValue;
-import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
@@ -24,7 +27,7 @@ public class MainActivity extends Activity implements HorizontalScrollViewListen
 
     static final float FONT_SIZE = 11;
     float mDeviceScale;
-    int mRowHeight;
+    int mRowDimension;
 
     private TableLayout mFrozenHeaderTable;
     private TableLayout mContentHeaderTable;
@@ -53,7 +56,7 @@ public class MainActivity extends Activity implements HorizontalScrollViewListen
         mContentScrollView.setHorizontalScrollBarEnabled(false); // Only show the scroll bar on the header table (so that there aren't two)
 
         mDeviceScale = getBaseContext().getResources().getDisplayMetrics().density;
-        mRowHeight = (int) (50 * mDeviceScale);
+        mRowDimension = (int) (50 * mDeviceScale);
 
         PopulateTempData();
     }
@@ -94,93 +97,100 @@ public class MainActivity extends Activity implements HorizontalScrollViewListen
         mFrozenTable.setBackgroundResource(R.color.lightGrey);
         mContentTable.setBackgroundResource(R.color.lightGrey);
 
+        // Setup table header
+        this.setupTableHeader(header);
+
+        // Setup table content
+        for (int i = 0; i < content.size(); i++) {
+            // Physician name in the frozen row.
+            Doctor p = content.get(i);
+            TableRow frozenRow = getTableRow();
+            TextView frozenCell = getTextView(p.physicianName);
+            frozenRow.addView(frozenCell);
+
+            // Appointment info in the rest of the sections.
+            TableRow row = getTableRow();
+            for (int j = 0; j < p.slots.size(); j++) {
+                View cell = getStatusView(p.slots.get(j));
+                row.addView(cell);
+            }
+
+            mFrozenTable.addView(frozenRow);
+            mContentTable.addView(row);
+        }
+
+        // Update content heights?
+        for (int i = 0; i < mContentTable.getChildCount(); i++) {
+            TableRow frozenRow = (TableRow) mFrozenTable.getChildAt(i);
+            frozenRow.setMinimumHeight(mRowDimension);
+            setChildTextViewWidths(frozenRow, 100);
+        }
+    }
+
+    /*
+     *   Sets up row-0 of the table with text headers.
+     */
+    private void setupTableHeader(ArrayList<String> header) {
+        TableRow frozenTableHeaderRow = getTableRow();
+        TextView frozenCell = getTextView("");
+        frozenTableHeaderRow.addView(frozenCell);
+
+        TableRow contentTableHeaderRow = getTableRow();
+        for (int j = 0; j < header.size(); j++) {
+            TextView rowCell = getTextView(header.get(j));
+            contentTableHeaderRow.addView(rowCell);
+        }
+
+        mFrozenHeaderTable.addView(frozenTableHeaderRow);
+        mContentHeaderTable.addView(contentTableHeaderRow);
+
+        frozenTableHeaderRow.setMinimumHeight(mRowDimension);
+        contentTableHeaderRow.setMinimumHeight(mRowDimension);
+
+        setChildTextViewWidths(frozenTableHeaderRow, 100);
+        setChildTextViewWidths(contentTableHeaderRow, 100);
+    }
+
+    private View getStatusView(DoctorAvailability availability) {
+        LayoutInflater inflater = (LayoutInflater)this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.view_doctor_availability, null);
+        ImageView imageView = (ImageView) findViewById(R.id.doctorAvailabilityViewStatus);
+        if (availability.status == TimeSlotStatus.Available) {
+//            view.setMinimumWidth(mRowDimension);
+//            view.setMinimumHeight(mRowDimension);
+//            imageView.setBackgroundResource(R.drawable.available_circular_shape);
+        }
+        else {
+//            view.setMinimumWidth(mRowDimension / 4);
+//            view.setMinimumHeight(mRowDimension / 4);
+//            imageView.setBackgroundResource(R.drawable.unavailable_circular_shape);
+        }
+        return view;
+    }
+
+    private TableLayout.LayoutParams getTableRowLayoutParams() {
         TableLayout.LayoutParams frozenRowParams = new TableLayout.LayoutParams(
                 TableLayout.LayoutParams.WRAP_CONTENT,
                 TableLayout.LayoutParams.WRAP_CONTENT);
         frozenRowParams.setMargins(1, 1, 1, 1);
         frozenRowParams.weight = 1;
-        TableLayout.LayoutParams tableRowParams = new TableLayout.LayoutParams(
-                TableLayout.LayoutParams.WRAP_CONTENT,
-                TableLayout.LayoutParams.WRAP_CONTENT);
-        tableRowParams.setMargins(0, 1, 1, 1);
-        tableRowParams.weight = 1;
+        return frozenRowParams;
+    }
 
-        TableRow frozenTableHeaderRow = null;
-        TableRow contentTableHeaderRow = null;
+    private TableRow getTableRow() {
+        TableRow frozenRow = new TableRow(this);
+        frozenRow.setLayoutParams(getTableRowLayoutParams());
+        frozenRow.setBackgroundResource(R.color.tableRows);
+        return frozenRow;
+    }
 
-        // Setup table content
-        for (int i = 0; i < content.size() + 1; i++) {
-            String frozenText = "";
-            ArrayList<String> data = new ArrayList<String>();
-            if (i == 0) {
-                // Header (0, 0) does not have any text.
-                frozenText = "";
-                data = header;
-            }
-            else {
-                Doctor p = content.get(i - 1);
-                ArrayList<DoctorAvailability> slots = p.slots;
-                frozenText = p.physicianName;
-                ArrayList<String> slotData = new ArrayList<String>();
-                for (int j = 0; j < slots.size(); j++) {
-                    slotData.add(slots.get(j).status.toString());
-                }
-                data = slotData;
-            }
-
-            TableRow frozenRow = new TableRow(this);
-            frozenRow.setLayoutParams(frozenRowParams);
-            frozenRow.setBackgroundResource(R.color.tableRows);
-
-            TextView frozenCell = new TextView(this);
-            frozenCell.setText(frozenText);
-            frozenCell.setTextColor(Color.parseColor("#FF000000"));
-            frozenCell.setPadding(5, 0, 5, 0);
-            frozenCell.setTextSize(TypedValue.COMPLEX_UNIT_DIP, FONT_SIZE);
-
-            frozenRow.addView(frozenCell);
-
-            // The rest of them
-            TableRow row = new TableRow(this);
-            row.setLayoutParams(tableRowParams);
-            row.setBackgroundResource(R.color.tableRows);
-
-            for (int j = 0; j < data.size(); j++) {
-                TextView rowCell = new TextView(this);
-                rowCell.setText(data.get(j));
-                rowCell.setPadding(10, 0, 0, 0);
-                rowCell.setGravity(Gravity.RIGHT);
-                rowCell.setTextColor(Color.parseColor("#FF000000"));
-                rowCell.setTextSize(TypedValue.COMPLEX_UNIT_DIP, FONT_SIZE);
-                row.addView(rowCell);
-            }
-
-            if (i == 0) {
-                frozenTableHeaderRow = frozenRow;
-                contentTableHeaderRow = row;
-                mFrozenHeaderTable.addView(frozenRow);
-                mContentHeaderTable.addView(row);
-            }
-            else {
-                mFrozenTable.addView(frozenRow);
-                mContentTable.addView(row);
-            }
-        }
-
-        frozenTableHeaderRow.setMinimumHeight(mRowHeight);
-        contentTableHeaderRow.setMinimumHeight(mRowHeight);
-
-        setChildTextViewWidths(frozenTableHeaderRow, 100);
-        setChildTextViewWidths(contentTableHeaderRow, 100);
-
-        for (int i = 0; i < mContentTable.getChildCount(); i++) {
-            TableRow frozenRow = (TableRow) mFrozenTable.getChildAt(i);
-            frozenRow.setMinimumHeight(mRowHeight);
-            setChildTextViewWidths(frozenRow, 100);
-            TableRow row = (TableRow) mContentTable.getChildAt(i);
-            row.setMinimumHeight(mRowHeight);
-            setChildTextViewWidths(row, 100);
-        }
+    private TextView getTextView(String text) {
+        TextView frozenCell = new TextView(this);
+        frozenCell.setText(text);
+        frozenCell.setTextColor(Color.parseColor("#FF000000"));
+        frozenCell.setPadding(5, 0, 5, 0);
+        frozenCell.setTextSize(TypedValue.COMPLEX_UNIT_DIP, FONT_SIZE);
+        return frozenCell;
     }
 
     private void setChildTextViewWidths(TableRow row, int width) {
@@ -196,6 +206,18 @@ public class MainActivity extends Activity implements HorizontalScrollViewListen
         }
     }
 
+    private void setChildViewWidths(TableRow row, int width) {
+        if (row == null) {
+            return;
+        }
+
+        for (int i = 0; i < row.getChildCount(); i++) {
+            View cell = (View) row.getChildAt(i);
+            int replacementWidth = (int) Math.ceil(width * mDeviceScale);
+            cell.setMinimumWidth(replacementWidth);
+        }
+    }
+
     private ArrayList<String> getTimeSlotHeader() {
         final Date start = getBeginningOfDay();
         final Date end = getEndOfDay();
@@ -204,7 +226,7 @@ public class MainActivity extends Activity implements HorizontalScrollViewListen
         cal.setTime(start);
         Date tmp = start;
         ArrayList<String> result = new ArrayList<String>();
-        DateFormat dateFormat = new SimpleDateFormat("HH:mm a");
+        DateFormat dateFormat = new SimpleDateFormat("hh:mm a");
         while (tmp.getTime() < end.getTime()) {
             result.add(dateFormat.format(tmp));
             cal.add(Calendar.MINUTE, 30);
